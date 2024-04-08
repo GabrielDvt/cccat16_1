7ccf6d9300e98d64a829c22f03d2a4e63c4aeb01
Gabriel Augusto De Vito <gabriel.dvt@hotmail.com>
2024-04-07 22:36:14 -0300

    refactoring

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import crypto from "crypto";
 import express from "express";
 import pgp from "pg-promise";
-import { validate } from "./validateCpf";
+import { validate } from "./validators/validateCpf";
+import { validateName } from "./validators/validateName";
+import { validateEmail } from "./validators/validateEmail";
 const app = express();
 app.use(express.json());
 
@@ -10,52 +12,74 @@ app.post("/signup", async function (req, res) {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
 		const id = crypto.randomUUID();
+		const name = req.body.name;
+		const email = req.body.email;
+		const cpf = req.body.cpf;
 
-		const [acc] = await connection.query("select * from cccat15.account where email = $1", [req.body.email]);
-		if (!acc) {
+		const [acc] = await connection.query(
+			"select * from cccat15.account where email = $1",
+			[req.body.email]
+		);
 
-			if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (req.body.email.match(/^(.+)@(.+)$/)) {
-
-					if (validate(req.body.cpf)) {
-						if (req.body.isDriver) {
-							if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, req.body.name, req.body.email, req.body.cpf, req.body.carPlate, !!req.body.isPassenger, !!req.body.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
+		if (acc) {
 			result = -4;
 		}
+
+		if (!validateName(name)) {
+			return -3;
+		}
+
+		if (!validateEmail(email)) {
+			return -2;
+		}
+
+		if (!validate(cpf)) {
+			return -1;
+		}
+
+		if (req.body.isDriver) {
+			if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+				await connection.query(
+					"insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
+					[
+						id,
+						req.body.name,
+						req.body.email,
+						req.body.cpf,
+						req.body.carPlate,
+						!!req.body.isPassenger,
+						!!req.body.isDriver,
+					]
+				);
+
+				const obj = {
+					accountId: id,
+				};
+				result = obj;
+			} else {
+				// invalid car plate
+				result = -5;
+			}
+		} else {
+			await connection.query(
+				"insert into cccat15.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
+				[
+					id,
+					req.body.name,
+					req.body.email,
+					req.body.cpf,
+					req.body.carPlate,
+					!!req.body.isPassenger,
+					!!req.body.isDriver,
+				]
+			);
+
+			const obj = {
+				accountId: id,
+			};
+			result = obj;
+		}
+
 		if (typeof result === "number") {
 			res.status(422).send(result + "");
 		} else {
